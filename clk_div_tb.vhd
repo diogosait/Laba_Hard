36aeb9d166bcb0bbb74f6f0fd46bf9f47e8fc952
Vitor Ramos <148400880+VitorRamos23@users.noreply.github.com>
2025-11-19 14:00:10 -0400

    Update clk_div_tb.vhd

--- clk_div_tb.vhd
+++ clk_div_tb.vhd
@@ -2,18 +2,18 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity tb_clock_1hz is
+entity clk_div_tb is
 end entity;
 
-architecture sim of tb_clock_1hz is
+architecture sim of clk_div_tb is
 
     signal clock_in  : std_logic := '0';
     signal clock_out : std_logic;
 
 begin
 
-    -- DUT: clock_1hz
-    dut: entity work.clock_1hz(rtl)
+    -- DUT: clk_div
+    dut: entity work.clk_div(rtl)
         generic map (
             DIVISOR => 10   -- bem pequeno só pra simulação
         )
@@ -25,10 +25,15 @@ begin
     -- Geração do clock de entrada (período 10 ns)
     clk_gen: process
     begin
-        clock_in <= '0';
-        wait for 5 ns;
-        clock_in <= '1';
-        wait for 5 ns;
+        
+        for i in natural range 0 to 50 loop
+            clock_in <= '0';
+            wait for 5 ns;
+            clock_in <= '1';
+            wait for 5 ns;
+            
+        end loop;
+        wait;
     end process;
 
 end architecture;
