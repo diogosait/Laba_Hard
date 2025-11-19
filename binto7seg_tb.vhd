d8086856d59e5274bcbb616b7027df21d422569b
Vitor Ramos <148400880+VitorRamos23@users.noreply.github.com>
2025-11-19 13:59:39 -0400

    Update binto7seg_tb.vhd

--- binto7seg_tb.vhd
+++ binto7seg_tb.vhd
@@ -2,10 +2,10 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity tb_binto7seg is
+entity binto7seg_tb is
 end entity;
 
-architecture sim of tb_binto7seg is
+architecture sim of binto7seg_tb is
 
     signal input_s  : std_logic_vector(3 downto 0) := (others => '0');
     signal display  : std_logic_vector(7 downto 0);
